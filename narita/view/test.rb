f8d1c736517f96ba44c39cbac4b177bc69f88b3f
Taiki Narita <nari@Narita-no-MacBook-Air.local>
2016-11-14 14:59:32 +0900

    POSCAR_2223

--- narita/view/test.rb
+++ narita/view/test.rb
@@ -3,7 +3,6 @@ require 'cairo'
 require 'scanf'
 
 lines = File.readlines(ARGV[0])
-
 lattice = []
 lines[2..4].each{|line|
   lattice << line.scanf("%f %f %f\n")
@@ -15,8 +14,6 @@ lines[7..a_max].each{|line|
   atom << line.scanf("%f %f %f\n")
 }
 
-
-
 real_pos=[]
 atom.each{|i|
  rpos=[0.0,0.0,0.0]
@@ -25,11 +22,24 @@ atom.each{|i|
    rpos[0] += a*lx  #原子座標 x 格子座標
    rpos[1] += a*ly
    rpos[2] += a*lz
-    
   }
   real_pos <<  rpos 
  }
 # p real_pos
+
+=begin
+ real_pos=[]
+ atom.each{|i|
+ rpos=[0.0,0.0,0.0]
+ i.each_with_index{|a,j|
+ 3.times{|k|
+ rpos[k] += a*lattice[j]
+  }
+ }
+ real_pos <<  rpos
+}
+ p real_pos
+=end
 
 width,height = 300,200
 cx,cy = width/2.0,height/2.0
@@ -45,26 +55,33 @@ context.set_source_rgb(0.9, 0.9, 0.9)
 context.rectangle(0, 0, width, height)
 context.fill
 
-#atomic
-context.set_source_rgb(1, 0, 0)
-real_pos.each{|pos|
-#  p pos
-#  p width*scale*pos[0] , height*scale*pos[1]
-#  p cx+adjust*scale*pos[0], cy+adjust*scale*pos[1]
-  context.circle(cx+adjust*scale*pos[0], cy+adjust*scale*pos[1], r)
-  context.fill}
-
 #coordinate
 context.set_source_rgb(0, 0, 0)
 [[0,1.0],[1.0,0]].each{|line|
-      x,y=line[0],line[1]
-      context.move_to(cx,cy)
-      context.line_to(cx+x*scale,cy+y*scale)
-      context.stroke
+    x,y=line[0],line[1]
+    context.move_to(cx,cy)
+    context.line_to(cx+x*scale,cy+y*scale)
+    context.stroke
+}
+
+#atomic
+real_pos.each{|pos|
+    
+#   atomic_x = cx+adjust*scale*pos[0]
+   atomic_y = cy+adjust*scale*pos[1]
+   atomic_z = cx+adjust*scale*pos[2]
+  p atomic_y, atomic_x
+#  d = (atomic_x - atomic_x)
+   context.circle(atomic_y,atomic_z, r)
+#  if(d < r){
+#      context.set_source_rgb(0, 0, 1)
+#      context.fill
+#  }
+#  else {
+      context.set_source_rgb(1, 0, 0)
+      context.fill
+#  }
 }
 
 context.show_page
 surface.finish
-
-
-
